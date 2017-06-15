7e18d75c3a07edc3873cd166b3ad5f0cef0a3ac5
antonror <hitty@i.ua>
2017-06-15 18:04:08 +0200

    Added bootstrap, sass; Gems: simple forms, bootstrap-sass; Generated: Book Model
    Added: Bookc creation form Changed: Gems to match env.
    
    Added: Book resources
    
    Added: Improved Book CRUD actions
    
    Added: Devise Gem
    
    Added: Devise Gem
    
    Added: Navbar
    
    Added: Navbar improvements on "if_user_signed_in?" via devise helper
    
    Data Modelling: Assigned a book to a current user via one-to-many relations; Added: Prevent user from deleting other user's books
    
    Data Modelling: Added Category Id to a Book; Added: Category model; Book and Category relations
    
    Added (Frontend): Category Dropdown to Navbar
    
    Improved (Frontend): Category Dropdown active if user is on selected category
    
    Category management (#1)
    
    * Initialized Branch Management
    
    * Added: Categories Controller; Rake resources :categories
    
    * Added: Index View; Controller Actions: index, new, create
    
    * Added: Form for Categories Creation; REFACTORED: render books/form, render categories/form
    
    * Improved: CRUD actions for  Form for Categories Creation; REFACTORED: render books/form, render categories/form
    
    * Added: VALIDATIONS on forms; Improved: CRUD actions and dependencies on Categories
    
    * Ready to merge
    
    Added: Paperclip Gem
    
    Finished: Frontend, CSS, Overall
    
    Refactor: Category Form styling, Category List styling
    
    Added: Notice disappear CSS Animation; Refactored: Categories Dropdown and Signup Form
    
    Final: Ready fot Heroku Deploy
    
    Final: POSTGRESQL Ready fot Heroku Deploy
    
    Improved: 'rails_12factor'
    
    Heroke
    
    Improved: Signup button removed on current user session
    
    Finalized: Heroku Deployment
    
    Prevent from adding multiple categories
    
    Prevent from adding multiple categories

---
+++ app/models/book.rb
@@ -0,0 +1,12 @@
+class Book < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category
+
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :author, :presence => true
+
+  has_attached_file :book_img, :styles => { :book_index => "250x350>", :book_show => "325x475>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :book_img, :content_type => /\Aimage\/.*\Z/
+
+end
