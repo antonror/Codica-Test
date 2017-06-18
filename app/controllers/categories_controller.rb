fabab66d128c1932cf8fa174c8863d07611edee2
antonror <hitty@i.ua>
2017-06-18 02:14:24 +0200

    Heroke

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,43 @@
+class CategoriesController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:name].blank?
+    @categories = Category.all.order("name ASC")
+      end
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @categories.destroy
+    redirect to categories_url
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+
+end
+
